5e4e6e160fd32359bbdb264abe87c59a73232df0
aleksejklimkin <lexklim91@gmail.com>
2026-06-16 08:38:29 +0300

    4 sprint_fixed

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 1) test_add_new_book_name_too_long_result_not_add(): тест проверяет, что не добавляеться книга с именем более 42 символов;
    
-2) test_set_book_genre_success_return_not_genre(): тест проверяет, что в добавленной книге отсутсвует жанр;
+2) test_book_genre_success_return_not_genre(): тест проверяет, что в добавленной книге отсутсвует жанр;
 
 3) test_set_book_genre_success():тест проверяет добавление жанра в список по имени книги;
 
@@ -12,12 +12,17 @@
             
 6) test_get_books_with_specific_genre_success():тест проверяет вывод книг по жанру;        
     
-7) test_get_books_genre_return_good_result_retern_good_list():тест проверяет вывод списка книг ввиде листа, по названию и жанру;
+7) test_get_books_genre_return_good_result_retern_list():тест проверяет вывод списка книг ввиде листа, по названию и жанру;
 
-8) test_get_books_for_children_return_result() тест проверяет отсутсвие добавления книг с жанрами 'Ужасы' и 'Детективы' в список для детей;
+8) test_get_books_for_children_good_list_return_result тест проверяет добавления книг в список для детей;
+
+9) test_get_books_for_children_bad_list_not_return(): тест проверяет отсутсвие добавления книг с жанрами
+"Ужасы" и "Детективы" в список для детей;
                    
-9) test_add_book_in_favorites_return_result_found() тест проверяет добавление книги в список избранные;
+10) test_add_book_in_favorites_return_result_found() тест проверяет добавление книги в список "избранные";
 
-10) test_delete_book_in_favorites_return_result_not_in_list() тест проверяет удаление книги из списка избранные;
+11) test_add_book_in_favorites_return_result_notfound():тест проверяет отсутсвие не добавленой книги в списоке "избранные";
 
-11) test_get_list_of_favorites_books_return_list() тест проверяет вывод списка книг из избранных 
+12) test_delete_book_in_favorites_return_result_not_in_list(): тест проверяет удаление книги из списка "избранные";
+
+13) test_get_list_of_favorites_books_return_list() тест проверяет вывод списка книг из "избранных" 
--- test_books_collector.py
+++ test_books_collector.py
@@ -2,47 +2,18 @@ import pytest
 from books_collector import BooksCollector
 
 class TestBooksbooks_collector:
-
-    @pytest.fixture
-    def books_collector(self):
-        return BooksCollector()
     
-    
-    books = [
-        ["Винни-Пух", "Мультфильмы"],
-        ["Сияние", "Ужасы"],
-        ["Убийство в Восточном экспрессе", "Детективы"],
-        ["Трое в лодке, не считая собаки", "Комедии"]
-    ]
-    
-    
-    @pytest.fixture
-    def books_collector_add(self, books_collector):       
-        books_collector.add_new_book("Дюна")
-        books_collector.add_new_book("Трое в лодке, не считая собаки")
-        books_collector.add_new_book("Винни-Пух")
-        books_collector.add_new_book("Сияние")
-        
-    @pytest.fixture 
-    def books_collector_genre(self, books_collector):
-        books_collector.set_book_genre("Дюна", "Фантастика")
-        books_collector.set_book_genre("Трое в лодке, не считая собаки", "Комедии")
-        books_collector.set_book_genre("Винни-Пух" ,"Мультфильмы")
-        books_collector.set_book_genre("Сияние","Ужасы")
-
-
-    def test_add_new_book_name_too_long_result_not_add(self, books_collector):
+    def ttest_add_new_book_name_too_long_result_not_add(self, books_collector):
         long_name = "А" * 42
         books_collector.add_new_book(long_name)
         assert long_name not in books_collector.books_genre
 
        
-    def test_set_book_genre_success_return_not_genre(self, books_collector):
+    def test_book_genre_success_return_not_genre(self, books_collector):
         books_collector.add_new_book("Сияние")
         assert books_collector.books_genre["Сияние"] == ""
 
-
-    @pytest.mark.parametrize("book_name, gener",[["Винни-Пух", "Мультфильмы"], ["Сияние", "Ужасы"],])
+    @pytest.mark.parametrize("book_name, gener",[["Винни-Пух", "Мультфильмы"], ["Сияние", "Ужасы"]])
     def test_set_book_genre_success(self, books_collector, book_name, gener ):
         books_collector.add_new_book(book_name)
         books_collector.set_book_genre(book_name, gener)
@@ -54,7 +25,7 @@ class TestBooksbooks_collector:
         assert "Несуществующая книга" not in books_collector.books_genre
     
     
-    @pytest.mark.parametrize("book_name, gener",books)
+    @pytest.mark.parametrize("book_name, gener",[["Винни-Пух", "Мультфильмы"], ["Сияние", "Ужасы"]])
     def test_get_book_genre_existing_book(self, books_collector, book_name, gener):
         books_collector.add_new_book(book_name)
         books_collector.set_book_genre(book_name, gener)
@@ -66,20 +37,24 @@ class TestBooksbooks_collector:
         assert result == ["Дюна"]
     
     
-    def test_get_books_genre_return_good_result_retern_good_list(self, books_collector, books_collector_add, books_collector_genre):
+    def test_get_books_genre_return_good_result_retern_list(self, books_collector, books_collector_add, books_collector_genre):
         result = books_collector.get_books_genre()
         assert result == { "Дюна":'Фантастика', "Трое в лодке, не считая собаки":"Комедии", "Винни-Пух":"Мультфильмы","Сияние":"Ужасы"}
     
         
-    @pytest.mark.parametrize("book_name, gener",books)    
-    def test_get_books_for_children_return_result(self, books_collector, book_name, gener ):        
+    @pytest.mark.parametrize("book_name, gener",[["Винни-Пух", "Мультфильмы"], ["Трое в лодке, не считая собаки", "Комедии"]]) 
+    def test_get_books_for_children_good_list_return_result(self, books_collector, book_name, gener ):        
         books_collector.add_new_book(book_name)
         books_collector.set_book_genre(book_name, gener)
         books_for_children = books_collector.get_books_for_children()
-        if  books_collector.get_book_genre(book_name)  not in books_collector.genre_age_rating:
-            assert book_name in books_for_children
-        else:
-            assert book_name not in books_for_children
+        assert book_name in books_for_children
+        
+    @pytest.mark.parametrize("book_name, gener",[["Убийство в Восточном экспрессе", "Детективы"], ["Сияние", "Ужасы"]])  
+    def test_get_books_for_children_bad_list_not_return(self, books_collector, book_name, gener ):        
+        books_collector.add_new_book(book_name)
+        books_collector.set_book_genre(book_name, gener)
+        books_for_children = books_collector.get_books_for_children()
+        assert book_name not in books_for_children
                    
     
     def test_add_book_in_favorites_return_result_found(self, books_collector, books_collector_add):
@@ -88,15 +63,20 @@ class TestBooksbooks_collector:
         assert name in books_collector.favorites
     
         
+    def test_add_book_in_favorites_return_result_notfound(self, books_collector, books_collector_add):
+        name = "Винни-Пух"
+        books_collector.add_book_in_favorites("Дюна")
+        assert name not in books_collector.favorites
         
-    def test_delete_book_in_favorites_return_result_not_in_list(self, books_collector, books_collector_add):
+        
+    def test_delete_book_in_favorites_return_bad_result(self, books_collector, books_collector_add):
         name = "Винни-Пух"
         books_collector.add_book_in_favorites(name)
         books_collector.delete_book_from_favorites(name)
         assert name not in books_collector.favorites
         
         
-    def test_get_list_of_favorites_books_return_list(self, books_collector, books_collector_add):
+    def test_get_list_of_favorites_books_result(self, books_collector, books_collector_add):
         books_collector.add_book_in_favorites("Дюна")
         books_collector.add_book_in_favorites("Винни-Пух")
         result = books_collector.get_list_of_favorites_books()
